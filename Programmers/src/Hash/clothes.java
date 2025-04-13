708a6333ea76920d10fdac941436363bc183dad2
junhyeokkk <104557505+junhyeokkk@users.noreply.github.com>
2025-04-13 17:48:50 +0900

    clothes

--- Programmers/src/Hash/clothes.java
+++ Programmers/src/Hash/clothes.java
@@ -1,5 +1,28 @@
 package Hash;
 
+import java.util.*;
+
 public class clothes {
 
+    // 1. 종류마다 각 종류의 개수 + 1(안입는경우)
+    // 2. 각 종류의 경우의 수를 곱하기 
+    public int solution(String[][] clothes) {
+
+    	Map<String, Integer> cMap = new HashMap<>();
+    	
+    	for(String[] cloth : clothes) {
+    		String type = cloth[1];
+    		cMap.put(type, cMap.getOrDefault(type,0)+1);
+    	}
+    	
+    	int answer = 1;
+    	
+    	for(int count : cMap.values()) {
+    		answer *= count + 1;
+    	}
+    	
+    	return answer -1;
+ 
+    }
+	
 }
